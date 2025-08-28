9f6d71c0c00983f390113bdfb451295e02c7b22a
ZIUVAR RUIZ ALVAREZ <ziuvarruiz310688@correo.itm.edu.co>
2025-08-27 22:15:20 -0500

    Reintroducir error en HomeController para revertir

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace TuProyecto.Controllers
         [HttpGet("mensaje")]
         public IActionResult GetMensaje()
         {
-            return Ok("Hello World desde la nueva funcionalidad");
+            return Ok("Error en el mensaje");
         }
     }
 }
