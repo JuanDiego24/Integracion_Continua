a33ecd9264a1de0d6b4f7e0fe1386b1feb7a737e
monicapdiaz <monicadiaz468@gmail.com>
2025-08-29 01:13:04 -0500

    "Mensaje para Enrique #2"

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,9 +10,8 @@ namespace TuProyecto.Controllers
         public IActionResult GetMensaje()
         {
 
-            return Ok("Mensaje para Monica");
 
-            return Ok("Mensaje para Enrique");
+            return Ok("Mensaje para Enrique numero dos");
         }
     }
 }
